c82d47eb171a5f801f413d4e4b7a9e8d43f809bc
nitesh-ui <112393577+nitesh-ui@users.noreply.github.com>
2022-08-30 05:45:55 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,4 @@
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import Destructing from './components/Destructing';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
 import ClickCounter from './components/HigherOrderComponent/ClickCounter';
 import HoverCounter from './components/HigherOrderComponent/HoverCounter';
 
@@ -15,16 +6,6 @@ function App() {
   return (
     <div className="App">
       <h4>Welcome to React JS Learning</h4>
-      <Welcome/>
-      <Greet name="Nitesh" age="28"/>
-      <Greet name="Madhav" age="30"/>
-      <Message/>
-      <Counter/>
-      <Destructing name="Pralay" heroName="Sachin"/>
-      <FunctionClick/>
-      <ClassClick/>
-      <EventBind/>
-      <ParentComponent/>
       <div>
         <p>Higher Order Component Example</p>
         <ClickCounter name="Nitesh"/>
